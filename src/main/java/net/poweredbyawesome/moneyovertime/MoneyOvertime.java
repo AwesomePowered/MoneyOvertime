69b763b74a5641c98c5890f0731c3bd3378a9983
U-NIFLHEIM\Lax <LaxWasHere@PoweredByHate.net>
2017-06-01 20:54:24 -0700

    Let there be time

---
+++ src/main/java/net/poweredbyawesome/moneyovertime/MoneyOvertime.java
@@ -0,0 +1,114 @@
+package net.poweredbyawesome.moneyovertime;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.logging.Level;
+
+public final class MoneyOvertime extends JavaPlugin implements Listener {
+
+    private static Economy econ = null;
+    int time;
+    public HashMap<UUID, Integer> invoice = new HashMap<>();
+    public HashMap<String, Permission> perms = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        time = getConfig().getInt("Time") * 20;
+        loadPerms();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        if (!setupEconomy()) {
+            getLogger().log(Level.WARNING, "No vault plugin found, only commands will work!");
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent ev) {
+        Player p = ev.getPlayer();
+        maekMoneh(p, getBasicTime(p));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent ev) {
+        Player p = ev.getPlayer();
+        spendMoneh(p);
+    }
+
+
+    public void maekMoneh(Player p, int rtime) {
+        while (!invoice.containsKey(p.getUniqueId())) { //while or if? ¯\_(ツ)_/¯
+            if (hasWildcard(p)) {
+                getLogger().log(Level.INFO, p.getName() + " has a wildcard permission ¯\\_(ツ)_/¯");
+                return;
+            }
+            invoice.put(p.getUniqueId(),Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+                for (String s : getConfig().getConfigurationSection("Deposits").getKeys(false)) {
+                    if (p.hasPermission(perms.get(s))) {
+                        if (econ != null) {
+                            econ.depositPlayer(p, getConfig().getInt("Deposits."+s+".amount"));
+                        }
+                        if (getConfig().getStringList("Deposits."+s+".commands") != null) {
+                            for (String command : getConfig().getStringList("Deposits."+s+".commands")) {
+                                getServer().dispatchCommand(Bukkit.getConsoleSender(), command.replace("%player%",p.getName()));
+                            }
+                        }
+                    }
+                }
+            },rtime,rtime));
+        }
+    }
+
+    public void spendMoneh(Player p) {
+        if (invoice.containsKey(p.getUniqueId())) {
+            Bukkit.getScheduler().cancelTask(invoice.get(p.getUniqueId()));
+            invoice.remove(p.getUniqueId());
+        }
+    }
+
+    public int getBasicTime(Player p) {
+        for (String s : getConfig().getConfigurationSection("Deposits").getKeys(false)) {
+            if (p.hasPermission("money.overtime."+s)) {
+                if (getConfig().getString("Deposits."+s+"time") != null) {
+                    return getConfig().getInt("Deposits."+s+"time");
+                }
+            }
+        }
+        return time;
+    }
+
+
+    public void loadPerms() {
+        perms.put("ireallyknowwhatimdoingiswear", new Permission("I.Really.Know.What.Im.Doing.I.Swear", PermissionDefault.FALSE));
+        for (String s : getConfig().getConfigurationSection("Deposits").getKeys(false)) {
+            perms.put(s, new Permission("money.overtime."+s, PermissionDefault.FALSE));
+        }
+    }
+
+    public boolean hasWildcard(Player p) { //so it has come to this, building around stupid people.
+        return p.hasPermission(perms.get("ireallyknowwhatimdoingiswear"));
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+}
